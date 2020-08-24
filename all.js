7851a5faa8f85d553bf62a3fc15ccab124cf5af2
Lu-ber <69476391+Lu-ber@users.noreply.github.com>
2020-08-24 13:59:54 +0800

    Initial commit

---
+++ all.js
@@ -0,0 +1,155 @@
+$(document).ready(function() {
+    $(".navbar-nav li").on("click", function() {
+        $(".navbar-nav").find(".active").removeClass("active");
+        $(this).parent("li").addClass("active");
+    });
+
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    $('#editModal').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
+            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text(recipient)
+    })
+
+    $('#deleteModal').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
+            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text(`確認刪除${recipient}`)
+    })
+})
+
+
+
+
+
+
+window.chartColors = {
+    red: "rgb(255, 99, 132)",
+    orange: "rgb(255, 159, 64)",
+    yellow: "rgb(255, 205, 86)",
+    green: "rgb(75, 192, 192)",
+    blue: "rgb(54, 162, 235)",
+    purple: "rgb(153, 102, 255)",
+    grey: "rgb(201, 203, 207)"
+};
+
+var randomScalingFactor = function() {
+    return Math.round(Math.random() * 100);
+};
+
+var config = {
+    type: "pie",
+    data: {
+        datasets: [{
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ],
+            backgroundColor: [
+                window.chartColors.red,
+                window.chartColors.orange,
+                window.chartColors.yellow,
+                window.chartColors.green,
+                window.chartColors.blue
+            ],
+            label: "Dataset 1"
+        }],
+        labels: ["Red", "Orange", "Yellow", "Green", "Blue"]
+    },
+    options: {
+        responsive: true
+    }
+};
+
+// bar
+var MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+var colors = Chart.helpers.color;
+var barChartData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+            label: "Dataset 1",
+            backgroundColor: colors(window.chartColors.red).alpha(0.5).rgbString(),
+            borderColor: window.chartColors.red,
+            borderWidth: 1,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ]
+        },
+        {
+            label: "Dataset 2",
+            backgroundColor: colors(window.chartColors.blue).alpha(0.5).rgbString(),
+            borderColor: window.chartColors.blue,
+            borderWidth: 1,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ]
+        }
+    ]
+};
+
+window.onload = function() {
+    // pie
+    document.querySelectorAll(".chart-item").forEach(function(item) {
+        config.data.datasets.forEach(function(dataset) {
+            dataset.data = dataset.data.map(function() {
+                return randomScalingFactor();
+            });
+        });
+        var ctx = item.getContext("2d");
+        window.myPie = new Chart(ctx, config);
+    });
+
+    // bar
+    var barCtx = document.getElementById("barCanvas").getContext("2d");
+    window.myBar = new Chart(barCtx, {
+        type: "bar",
+        data: barChartData,
+        options: {
+            responsive: true,
+            legend: {
+                position: "top"
+            },
+            title: {
+                display: true,
+                text: "Chart.js Bar Chart"
+            }
+        }
+    });
+};
